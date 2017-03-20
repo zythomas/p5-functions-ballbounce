314af4b7f273c8c4e88e3344fda315d5beee526e
Jordan Budisantoso <jbudisantoso@wlapcs.org>
2017-03-20 14:26:01 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var x = 300;
+var y = 200;
+var speedx = 8;
+var speedy = 6;
+
+
+function setup(){
+    createCanvas(600,400);
+}
+
+function draw(){
+    background(0);
+
+    //display ball
+    stroke(255);
+    strokeWeight(4);
+    fill(50,250,50);
+    ellipse(x, y, 50, 50);
+
+    
+    //make ball move
+    y = y + speedy;
+    x = x + speedx;
+    
+    //ball changes direction when it hits wall
+    if(x>600 || x<0){
+        speedx = -speedx;
+    }
+     if(y>400 || y<0){
+        speedy = -speedy;
+    }
+}
